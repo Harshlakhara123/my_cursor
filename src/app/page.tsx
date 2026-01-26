343a504d119e2e0912d0dd5ea25b87f517568721
Harshlakhara123 <lakharaharsh65@gmail.com>
2026-01-26 20:29:55 +0530

    03: database setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,25 @@
+"use client";
+import { useMutation, useQuery }  from "convex/react";
+import { api} from "../../convex/_generated/api";
+import { use } from "react";
 import { Button } from "@/components/ui/button";
-import { Ghost } from "lucide-react";
 
 const x = ()=>{
+  const projects = useQuery(api.projects.get);
+  const createProject = useMutation(api.projects.create);
     return (
-        <div>
-          <Button>
-            click me
+        <div className="flex flex-col gap-2 p-4">
+          <Button onClick={() => createProject({
+            name: "New Project123",
+          })}>
+           Add new 
           </Button>
+          {projects?.map((project) => (
+            <div className="border rounded p-2 flex flex-col" key={project._id}>
+              <p>{project.name}</p>
+              <p>Owner Id: {project.ownerId}</p>
+            </div>
+          ))}
         </div>
     );
 };
